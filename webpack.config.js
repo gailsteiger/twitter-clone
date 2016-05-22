671c163208ec7838f7e759c2048407869cd1b3fc
gail <gsteiger@netvision.net.il>
2016-05-22 23:49:15 +0300

    init app, client code and webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+/**
+ * Created by gail on 5/14/16.
+ */
+
+module.exports = {
+    entry: "./app/assets/frontend/main.jsx",
+    output: {
+        path: __dirname + "/app/assets/javascripts",
+        filename: "bundle.js"
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    }
+};
