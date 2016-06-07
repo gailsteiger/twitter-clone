ed538093dac772ee78122072b164e6584b2f46b2
gail <gsteiger@netvision.net.il>
2016-06-08 00:12:42 +0300

    init app, client code and webpack comonents, mockup hook form up
    user login and authentication
    api
    flux

---
+++ app/assets/frontend/actions/TweetActions.jsx
@@ -0,0 +1,9 @@
+// all actions having to do with writing and reading tweets
+
+import API from "../API"
+
+export default {
+    getAllTweets() {
+        API.getAllTweets();  
+    }
+}
---
+++ app/assets/frontend/stores/TweetStore.jsx
@@ -0,0 +1,15 @@
+import AppDispatcher from '../dispatcher'
+import ActionTypes from '../constants'
+
+AppDispatcher.register( action => {
+    switch (action.actionType) {
+        case ActionTypes.RECEIVED_TWEETS:
+            //
+            
+            break;
+        
+        default:
+            // no op
+            break;
+    }
+});
